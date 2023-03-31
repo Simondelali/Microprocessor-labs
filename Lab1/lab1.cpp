2b8786308ebe214804097075d45ee5287217778d
Simon Atiegar <atiegardelali@gmail.com>
2023-03-31 20:07:53 +0000

    add lab1

--- Lab1/lab1.cpp
+++ Lab1/lab1.cpp
@@ -1,9 +1,27 @@
-void setup(){
-    int RED = 12;
-    int YELLOW = 13;
-    int GREEN = 14;
+int redLED = 8;
+int yellowLED = 12;
+int greenLED = 13;
+void setup()
+{
+  pinMode(redLED, OUTPUT);
+  pinMode(yellowLED, OUTPUT);
+  pinMode(greenLED, OUTPUT);
 }
 
-
-void loop(){
+void loop()
+{
+  digitalWrite(redLED, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(redLED, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  
+  digitalWrite(yellowLED, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(yellowLED, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  
+  digitalWrite(greenLED, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(greenLED, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
 }
